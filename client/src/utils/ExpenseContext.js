d50b4ef06b1132b877820c04ed88cef6080613e0
Bryant Krause <bkrause@ttdwest.local>
2020-02-25 12:27:48 -0800

    updated proxy... started on table display

---
+++ client/src/utils/ExpenseContext.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import axios from 'axios'
+
+const ExpensePage = props => {
+
+    const [state, setState] = React.useState({
+
+      invoice: [],
+      amount: '',
+      account: [],
+      class: [],
+      date: '',
+      description: '',
+      randomArr: [],
+      handleInputChange: () => {},
+      handleSubmitButton: () => {},
+      handleUpdateReport: () => { },
+      handleSubmitReport: () => { }, 
+      getInvoices: () => { },
+    })
+
+
+}
+
+export default ExpensePage
