92d7b5196ad60eb65b4364795e27fb7af5569436
Bryant Krause <bryantkrause@gmail.com>
2020-03-02 21:30:29 -0800

    added table page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
+import { Container } from '@material-ui/core'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import ReportPage from './pages/ReportPage'
 
 
+function App(props) {
 
-function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ ReportPage } {...props} />
+          </Switch>
+      </BrowserRouter>
+    </Container>
+  )
+
+  
 }
 
-export default App;
+export default App
--- client/src/pages/ReportPage.js
+++ client/src/pages/ReportPage.js
@@ -1,3 +1,21 @@
 import React from 'react'
 import axios from 'axios'
-import ExpenseContext from '../../utils/ExpenseContext'
+import ExpenseContext from '../utils/ExpenseContext'
+import TableDisplay from '../../src/components/TableDisplays.js'
+
+const BoardsPage = _ => {
+
+    // const [boardState, setBoardState] = React.useState({
+    //   title: '',
+    //   description: '',
+    //   boards: []
+    // })
+
+    return (
+        <ExpenseContext.Provider>
+          <TableDisplay />
+        </ExpenseContext.Provider>
+      )
+    }
+    
+    export default BoardsPage
